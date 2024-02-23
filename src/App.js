3ad6f6936a53c53d6c1baa7ea13f0aafb05330c6
Deo Raj <d.jrajsingh81@gmail.com>
2024-02-23 23:55:40 +0530

    Some Changes Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp.jsx";
-import About from "./pages/About.jsx";
-import Profile from './pages/Profile.jsx'
+import Home from "./pages/Home.js";
+import SignIn from "./pages/SignIn.js";
+import SignUp from "./pages/SignUp.js";
+import About from "./pages/About.js";
+import Profile from './pages/Profile.js'
+import Header from "./components/Header.js";
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
