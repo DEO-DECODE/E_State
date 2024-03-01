622b6afa707818c0a8860856b9ef276cd7a66f11
Deo Raj <d.jrajsingh81@gmail.com>
2024-03-01 16:56:12 +0530

    Complete Image Upload Functionality Done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Home from "./pages/Home.js";
 import SignIn from "./pages/SignIn.js";
 import SignUp from "./pages/SignUp.js";
 import About from "./pages/About.js";
-import Profile from './pages/Profile.js'
+import Profile from "./pages/Profile.js";
 import Header from "./components/Header.js";
+import PrivateRoute from "./components/PrivateRoute.js";
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
