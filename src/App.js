75cb5884a39e2a4d4237b6ad6d1013428514bfe2
Deo Raj <d.jrajsingh81@gmail.com>
2024-03-07 13:23:44 +0530

    Complete Update Listing Functionality Done.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import About from "./pages/About.js";
 import Profile from "./pages/Profile.js";
 import Header from "./components/Header.js";
 import PrivateRoute from "./components/PrivateRoute.js";
-import CreateListing from './pages/CreateListing.js';
+import CreateListing from "./pages/CreateListing.js";
+import UpdateListing from './pages/UpdateListing.js';
 const App = () => {
   return (
     <BrowserRouter>
@@ -19,7 +20,11 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
