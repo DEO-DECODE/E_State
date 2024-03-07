3e9c20488dbc63ca4273521027185e290ed0e67c
Deo Raj <d.jrajsingh81@gmail.com>
2024-03-07 14:24:10 +0530

    Complete Contact Landlord Functionality Done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { test, updateUser , deleteUser, getUserListings} from "../controllers/userController.js";
+import { test, updateUser , deleteUser, getUserListings, getUser} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.get("/", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get('/:id', verifyToken, getUser)
 export default router;
